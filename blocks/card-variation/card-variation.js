a2883f7ee6968ec83e609c4831132e4dfe838581
nikhilrakala11 <159516508+nikhilrakala11@users.noreply.github.com>
2026-09-17 17:27:25 +0530

    cardrelated js kjkkllll

--- blocks/card-variation/card-variation.js
+++ blocks/card-variation/card-variation.js
@@ -1,12 +1,24 @@
 export default function decorate(block) {
+
     console.log('Decorating card variation block', block);
+
     const ul = document.createElement('ul');
+
     const rows = [...block.children];
-   rows.forEach((row) => {
-    const columns = [...row.children];
-        const li = document.createElement('li');
-        li.append(...columns);
-        ul.append(li);
-        console.log('Added row to ul', ul);
+
+    rows.forEach((row) => {
+
+        const columns = [...row.children];
+
+        const course = columns[0].textContent.trim();
+        const duration = columns[1].textContent.trim();
+        const location = columns[2].textContent.trim();
+        const cta = columns[3].textContent.trim();
+
+        console.log('COURSE:', course);
+        console.log('DURATION:', duration);
+        console.log('LOCATION:', location);
+        console.log('CTA:', cta);
+
     });
-  }
+}
