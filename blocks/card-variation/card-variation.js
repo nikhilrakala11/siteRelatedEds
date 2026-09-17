90c274a1d15d53325341657f76397b9691e2ed77
nikhilrakala11 <159516508+nikhilrakala11@users.noreply.github.com>
2026-09-17 13:09:46 +0530

    cardrelated js sec check

--- blocks/card-variation/card-variation.js
+++ blocks/card-variation/card-variation.js
@@ -2,12 +2,12 @@ export default function decorate(block) {
     console.log('Decorating card variation block', block);
     const ul = document.createElement('ul');
     const rows = [...block.children];
-
-    rows.forEach((row) => {
-    console.log('ROW:', row);
-
+   rows.forEach((row) => {
     const columns = [...row.children];
 
-    console.log('COLUMNS:', columns);
-    });
-}
+    console.log(columns[0]);
+    console.log(columns[1]);
+    console.log(columns[2]);
+    console.log(columns[3]);
+});
+    };
