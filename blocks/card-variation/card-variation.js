403c85b0c880bf775d452ad094e597846f539cce
nikhilrakala11 <159516508+nikhilrakala11@users.noreply.github.com>
2026-09-17 12:55:52 +0530

    cardrelated js

--- blocks/card-variation/card-variation.js
+++ blocks/card-variation/card-variation.js
@@ -1,4 +1,9 @@
 export default function decorate(block) {
-    console.log('Decorating card variation block',block);
+    console.log('Decorating card variation block', block);
+    const ul = document.createElement('ul');
+    const rows = [...block.children];
 
+    rows.forEach((row) => {
+        console.log(row);
+    });
 }
